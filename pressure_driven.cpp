1142de72e1135d521db71a0b9cb55fb1f21514c8
Kabeer120 <97871505+Kabeer120@users.noreply.github.com>
2023-11-14 17:25:35 +0530

    Update pressure_driven.cpp

--- pressure_driven.cpp
+++ pressure_driven.cpp
@@ -18,13 +18,13 @@ private:
     T* w;
     T* ex;
     T* ey;
-    T nu;
+    
     T omega;
     T dx;
     T dy;
-    T dt;
+    
     T rho0;
-    T tau;
+    
     T g;
     T t1;
     T t2;
@@ -35,42 +35,57 @@ private:
     T sy;
     T* uexact;
     T** shearStress;
-    T mach=;
-    T sound=;
-    T re;
-    T uxn;
+    double cs2 = 1.0/3.0;
+    double cs = sqrt(cs2);
+    double Ma = 0.01;
+    
+    double uMax = Ma*cs;
+    double Re = 100;
+    double Kn = Ma/Re;
+    double refLen = 1;
+    double nu = uBoundary*refLen/Re;
+    double tau = kinVisc/cs2;
+    double dt = refLen/n;
+    double tauNdim = tau/dt;
+    double beta = 1.0 / (2.0*tauNdim + 1.0);
+    int simulationTime = 3 /nu;
 
 public:
     LatticeBoltzmann(int nx, int ny)
-        : n(nx), m(ny), dx(1.0), dy(1.0), dt(1.0), uxn( mach*sound), nu(ux*ny/re) ,rho0(1.0), tau(ny*ny/nu), g() {
+        : N(nx), M(ny), dx(1.0), dy(1.0), dt(1.0), uxn( mach*sound), nu(ux*ny/re) ,rho0(1.0), tau(ny*ny/nu), g() {
+        int n = N+2;
+        int m = M + 2;
 
+        
+        
         f = new T**[9];
         feq = new T**[9];
         source = new T**[9];
         for (int k = 0; k < 9; k++) {
-            f[k] = new T*[n + 1];
-            feq[k] = new T*[n + 1];
-            source[k] = new T*[n + 1];
-            for (int i = 0; i <= n; i++) {
-                f[k][i] = new T[m + 1];
-                feq[k][i] = new T[m + 1];
-                source[k][i] = new T[m + 1];
+            f[k] = new T*[n ];
+            feq[k] = new T*[n ];
+            
+            for (int i = 0; i < n; i++) {
+                f[k][i] = new T[m ];
+                feq[k][i] = new T[m ];
+                
             }
         }
 
-        u = new T*[n + 1];
-        v = new T*[n + 1];
-        rho = new T*[n + 1];
-        for (int i = 0; i <= n; i++) {
-            u[i] = new T[m + 1];
-            v[i] = new T[m + 1];
-            rho[i] = new T[m + 1];
+        u = new T*[n ];
+        v = new T*[n ];
+        rho = new T*[n ];
+        for (int i = 0; i < n; i++) {
+            u[i] = new T[m ];
+            v[i] = new T[m ];
+            rho[i] = new T[m ];
         }
 
         w = new T[9];
         ex = new T[9];
         ey = new T[9];
-        uexact = new T[m + 1];
+        opp = new T[9];
+        uexact = new T[m ];
 
         w[0] = static_cast<T>(4.0 / 9.0);
         w[1] = static_cast<T>(1.0 / 9.0);
@@ -102,24 +117,22 @@ public:
         ey[7] = static_cast<T>(-1.0);
         ey[8] = static_cast<T>(-1.0);
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                u[i][j] = static_cast<T>(0.0);
-                v[i][j] = static_cast<T>(0.0);
-                rho[i][j] = static_cast<T>(rho0);
-                for (int k = 0; k < 9; k++) {
-                    source[k][i][j] = static_cast<T>(0.0);
+        for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        u[i][j] = static_cast<T>(0.0);
+                        v[i][j] = static_cast<T>(0.0);
+                        rho[i][j] = static_cast<T>(rho0);
+                        
+                    }
                 }
+        
+                nu = (tau - static_cast<T>(0.50)) / static_cast<T>(3.0);
+                omega = static_cast<T>(1.0) / ((static_cast<T>(3.0) * nu) + static_cast<T>(0.50));
             }
-        }
-
-        nu = (tau - static_cast<T>(0.50)) / static_cast<T>(3.0);
-        omega = static_cast<T>(1.0) / ((static_cast<T>(3.0) * nu) + static_cast<T>(0.50));
-    }
 
     void initializeF() {
-        for (int j = 0; j <= m; j++) {
-            for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
                 T t1 = u[i][j] * u[i][j] + v[i][j] * v[i][j];
                 for (int k = 0; k <= 8; k++) {
                     T t2 = u[i][j] * ex[k] + v[i][j] * ey[k];
@@ -131,84 +144,133 @@ public:
     }
 
     void collision(int steps) {
-        for (int t = 1; t <= steps; t++) {
-            // Collision
-            for (int i=0; i < m;i++){
-                for(int k =0 ; k <=8;k++{
-                    feq[k][i][0]=
-                   
+        for (int t = 1; t <= simulationTime; t++) {
+
+            // fiLLING GHOST NODE WITH EQUILBRIUM VALUES.
+            for(int i  =0 ; i < n;i++){
+                for(int k = 0 ; k <=8;k++){
+                    f[k][i][0] = (  w[k] )*(1.0 + 3.0*uBoundary*ex[k] + 4.5*(uBoundary*ex[k]*uBoundary*ex[k]) - 1.5*uBoundary*uBoundary);
                 }
             }
-           for(int j=0;j<n;j++){
-               for (int k=0;k<=8;k++){
-                    
-            
-            for (int j = 1; j <= m; j++) {
-                for (int i = 1; i <= n; i++) {
+            for(int i  =0 ; i < n;i++){
+                for(int k = 0 ; k <=8;k++){
+                    f[k][i][m] = (  w[k] )*(1.0 + 3.0*uBoundary*ex[k] + 4.5*(uBoundary*ex[k]*uBoundary*ex[k]) - 1.5*uBoundary*uBoundary);
+                }
+            }
+            for(int j  =0 ; i < m;i++){
+                for(int k = 0 ; k <=8;k++){
+                    f[k][0][j] = (  w[k] )*(1.0 + 3.0*uBoundary*ex[k] + 4.5*(uBoundary*ex[k]*uBoundary*ex[k]) - 1.5*uBoundary*uBoundary);
+                }
+            }
+            for(int j  =0 ; j < m;i++){
+                for(int k = 0 ; k <=8;k++){
+                    f[k][m][j] = (  w[k] )*(1.0 + 3.0*uBoundary*ex[k] + 4.5*(uBoundary*ex[k]*uBoundary*ex[k]) - 1.5*uBoundary*uBoundary);
+                }
+            }
+            // Collision
+            for (int j = 1; j < m-1; j++) {
+                for (int i = 1; i < n-1; i++) {
                     T t1 = u[i][j] * u[i][j] + v[i][j] * v[i][j];
                     for (int k = 0; k <= 8; k++) {
                         T t2 = u[i][j] * ex[k] + v[i][j] * ey[k];
                         T t3 = static_cast<T>(3.0) * (ex[k] - u[i][j]);
                         T t4 = static_cast<T>(9.0) * ((ex[k] * u[i][j]) + (ey[k] * v[i][j])) * ex[k];
-                        source[k][i][j] = (static_cast<T>(1.0) - static_cast<T>(0.50) / tau) * w[k] * g * (t3 + t4);
+                        
                         feq[k][i][j] = rho[i][j] * w[k] * (static_cast<T>(1.0) + (static_cast<T>(3.0) * t2) + (static_cast<T>(4.50) * t2 * t2) - (static_cast<T>(1.50) * t1));
                         f[k][i][j] = (omega * feq[k][i][j]) + ((static_cast<T>(1.0) - omega) * f[k][i][j]) ;
                     }
                 }
             }
-            for (int j = 0; j <= m; j++) {
-                for (int i = n; i >= 1; i--) {
+            
+            // Updating ghost nodes at 0th  column before advecting after every time step to maintain constant conditions.
+            //( 1- EAST, 2- NORTH, 3- WEST, 4- SOUTH,5 - NE, 6-NW,7- SW,8- SE )
+            //LEFT WALL GHOST UPDATE
+            for(int i = 0; i <n;i++){
+                f[3][i][0] = f[3][i][1];
+                f[6][i][0] = f[6][i][1];
+                f[7][i][0] = f[7][i][1];
+            }
+            //RIGHT WALL GHOST UPDATE
+
+            for(int i = 0 ; i <n;i++){
+                f[1][i][N+1] = f[1][i][N];
+                f[5][i][N+1] = f[5][i][N];
+                f[8][i][N+1] = f[8][i][N];
+
+            }
+
+            //Bottom WALL GHOST UPDATE
+
+            for(int j = 0 ; j < m; j++){
+                f[4][0][j] = f[4][1][j];
+                f[7][0][j] = f[7][1][j];
+                f[8][0][j] = f[8][1][j];
+
+            }
+            //TOP WALL GHJOST UPDATE
+
+            for(int j = 0 ; j < m; j++){
+                f[2][N+1][j] = f[2][N][j];
+                f[5][N+1][j] = f[5][N][j];
+                f[6][N+1][j] = f[6][N][j];
+
+            }
+
+
+
+            for (int j = 1; j < m-1; j++) {
+                for (int i = n-1; i >= 1; i--) {
                     f[1][i][j] = f[1][i - 1][j];
                 }
-                for (int i = 0; i <= n - 1; i++) {
+                for (int i = 1; i < n - 1; i++) {
                     f[3][i][j] = f[3][i + 1][j];
                 }
             }
             // Streaming.
-            for (int j = m; j >= 1; j--) {
-                for (int i = 0; i <= n; i++) {
+            for (int j =1; j <= m-1; j++) {
+                for (int i = n-1; i >=1; i--) {
                     f[2][i][j] = f[2][i][j - 1];
                 }
-                for (int i = n; i >= 1; i--) {
+                for (int i = n-1; i >= 1; i--) {
                     f[5][i][j] = f[5][i - 1][j - 1];
                 }
-                for (int i = 0; i <= n - 1; i++) {
+                for (int i = 1; i <= n - 1; i++) {
                     f[6][i][j] = f[6][i + 1][j - 1];
                 }
             }
-           
-                for (int i = 0; i <= n; i++) {
-                    f[4][1][j] = f[4][0][j];
+            for (int j = 1; j < m-1; j++) {
+                for (int i = 1; i < n-1; i++) {
+                    f[4][i][j] = f[4][i][j + 1];
                 }
-                for (int i = 0; i < n; i++) {
-                    f[7][1][j] = f[7][0][j];
+                for (int i = 1; i < n-1; i++) {
+                    f[7][i][j] = f[7][i + 1][j + 1];
                 }
-                for (int j= n; j >= 1; j--) {
-                    f[8][1][j] = f[6][0][j];
+                for (int i = n-1; i >= 1; i--) {
+                    f[8][i][j] = f[8][i - 1][j + 1];
                 }
-            
+            }
             // Boundary Conditions
             // North and South boundary - wall
-            for (int i = 0; i <= n; i++) {
-                f[2][i][0] = f[4][i][0];
-                f[5][i][0] = f[7][i][0];
-                f[6][i][0] = f[8][i][0];
-                f[4][i][m] = f[2][i][m];
-                f[7][i][m] = f[5][i][m];
-                f[8][i][m] = f[6][i][m];
+            for (int i = 1; i < n-1; i++) {
+                f[2][i][1] = f[4][i][1];
+                f[5][i][1] = f[7][i][1];
+                f[6][i][1] = f[8][i][1];
+                f[4][i][m-1] = f[2][i][m-1];
+                f[7][i][m-1] = f[5][i][m-1];
+                f[8][i][m-1] = f[6][i][m-1];
             }
-            for (int j = 0; j <= m; j++) {
+            for (int j = 1; j < m-1; j++) {
                 // West boundary - periodicity
-                f[1][0][j] = f[1][n][j];
-                f[5][0][j] = f[5][n][j];
-                f[8][0][j] = f[8][n][j];
+                f[1][1][j] = f[1][n-1][j];
+                f[5][1][j] = f[5][n-1][j];
+                f[8][1][j] = f[8][n-1][j];
                 // East boundary - periodicity
-                f[3][n][j] = f[3][0][j];
-                f[6][n][j] = f[6][0][j];
-                f[7][n][j] = f[7][0][j];
+                f[3][n-1][j] = f[3][1][j];
+                f[6][n-1][j] = f[6][1][j];
+                f[7][n-1][j] = f[7][1][j];
             }
-            for (int j = 0; j <= m; j++) {
-                for (int i = 0; i <= n; i++) {
+            for (int j = 1; j < m-1; j++) {
+                for (int i = 1; i < n-1; i++) {
                     T s = static_cast<T>(0.0);
                     for (int k = 0; k <= 8; k++) {
                         s += f[k][i][j];
@@ -216,8 +278,8 @@ public:
                     rho[i][j] = s;
                 }
             }
-            for (int j = 0; j <= m; j++) {
-                for (int i = 0; i <= n; i++) {
+            for (int j = 1; j < m-1; j++) {
+                for (int i = 1; i < n-1; i++) {
                     T sx = static_cast<T>(0.0);
                     T sy = static_cast<T>(0.0);
                     for (int k = 0; k <= 8; k++) {
@@ -230,9 +292,13 @@ public:
             }
         }
     }
+        
+          
+                   
 
-    void compute_exact() {
-        for (int j = 0; j <= m; j++) {
+
+void compute_exact() {
+        for (int j = 1; j < m-1; j++) {
         
             T y = static_cast<T>(j) / static_cast<T>(m) * m;
             
@@ -249,14 +315,19 @@ public:
         std::ofstream file1("field.txt");
         std::ofstream file2("comparison.txt");
 
-        for (int j = 0; j <= m; j++) {
-            for (int i = 0; i <= n; i++) {
-                file1 << i << " " << j << " " << u[i][j] << " " << v[i][j] << " " << rho[i][j] << std::endl;
+        for (int j = 1; j < m-1; j++) {
+            for (int i = 1; i < n-1; i++) {
+                int position = std::pow(i-circleX, 2)+std::pow(j-circleY, 2);
+                if ( position > circleR2){
+                    file1 << i << " " << j << " " << u[i][j] << " " << v[i][j] << " " << rho[i][j] << std::endl;
+
+                }
+                
             }
             file1 << std::endl;
         }
 
-        for (int j = 0; j <= m; j++) {
+        for (int j = 0; j < m-1; j++) {
             file2 << static_cast<double>(j) / m << " " << u[60][j] << " " << v[60][j] << " " << uexact[j] << std::endl;
         }
 
@@ -264,15 +335,15 @@ public:
         file2.close();
     }
     void initializeShearStress() {
-        shearStress = new T*[n + 1];
-        for (int i = 0; i <= n; i++) {
-            shearStress[i] = new T[m + 1];
-            memset(shearStress[i], 0, sizeof(T) * (m + 1));
+        shearStress = new T*[n ];
+        for (int i = 0; i < n-1; i++) {
+            shearStress[i] = new T[m ];
+            memset(shearStress[i], 0, sizeof(T) * (m));
         }
     }
     void computeShearStress() {
-        for (int j = 1; j < m; j++) {
-            for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m-1; j++) {
+            for (int i = 1; i < n-1; i++) {
                 
                 T dudx = static_cast<T>((u[i + 1][j] - u[i - 1][j]) / (2.0 * dx));
 
@@ -287,8 +358,9 @@ public:
     
 
     
-    for (int j = 0; j <= m; j++) {
-        for (int i = 0; i <= n; i++) {
+    for (int j = 1; j < m-1; j++) {
+        for (int i = 1; i < n-1; i++) {
+
             file << i << " " << j << " " << shearStress[i][j] << endl;
         }
     }
@@ -301,11 +373,11 @@ public:
 int main() {
     int nx = 100;
     int ny = 30;
-    LatticeBoltzmann<long long> lbm(nx, ny);
-    int time = 10000;
+    LatticeBoltzmann<int> lbm(nx, ny);
+    
 
     lbm.initializeF();
-    lbm.collision(time);
+    lbm.collision();
     lbm.compute_exact();
     lbm.generate_data();
 
@@ -313,85 +385,6 @@ int main() {
     lbm.computeShearStress();
     lbm.saveShearStressData();
 
+
     return 0;
-}
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-// Define the lattice size
-const int nx = 100;
-const int ny = 100;
-
-// Define the fluid properties
-const double rho = 1.0;
-const double nu = 0.01;
-
-// Define the pressure gradient
-const double dp = 1.0;
-
-// Define the boundary conditions
-const bool periodicX = true;
-const bool periodicY = true;
-
-// Declare the lattice
-vector<double> f[nx][ny][9];
-
-// Initialize the lattice
-for (int i = 0; i < nx; i++) {
-  for (int j = 0; j < ny; j++) {
-    for (int k = 0; k < 9; k++) {
-      f[i][j][k] = 1.0 / 9.0;
-    }
-  }
-}
-
-// Calculate the collision step
-for (int i = 0; i < nx; i++) {
-  for (int j = 0; j < ny; j++) {
-    for (int k = 0; k < 9; k++) {
-      f[i][j][k] = f[i][j][k] - (3.0 * dp * dt) / (2.0 * nu) * (f[i][j][k] - f_eq[i][j][k]);
-    }
-  }
-}
-
-// Calculate the streaming step
-for (int i = 0; i < nx; i++) {
-  for (int j = 0; j < ny; j++) {
-    for (int k = 0; k < 9; k++) {
-      f[(i + dx[k]) % nx][(j + dy[k]) % ny][k] = f[i][j][k];
-    }
-  }
-}
-
-// Print the results
-for (int i = 0; i < nx; i++) {
-  for (int j = 0; j < ny; j++) {
-    cout << f[i][j][0] << " ";
-  }
-  cout << endl;
-}
-
-int main() {
-  // Initialize the simulation
-  double dt = 0.1;
-  int t = 0;
-
-  // Run the simulation for 100 time steps
-  while (t < 100) {
-    // Calculate the collision step
-    calculateCollisionStep(f, dt);
-
-    // Calculate the streaming step
-    calculateStreamingStep(f, dx, dy);
-
-    // Increment the time step
-    t++;
-  }
-
-  // Print the results
-  printResults(f);
-
-  return 0;
 }
